a1d2c7015698785bfd0058f3fcc9a2b69715f9f7
Jeevika Sirwani <jeevika.sirwani4@gmail.com>
2025-05-20 15:12:21 +0530

    .

--- backend/src/controllers/postController.ts
+++ backend/src/controllers/postController.ts
@@ -2,100 +2,164 @@ import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 import { Context } from "hono";
-
+import { postInputschema, postUpdateschema } from "../../../common/src/index";
 
 export const blogRouter = new Hono<{
-    Bindings: {
-        DATABASE_URL: string;
-        JWT_SECRET: string;
-    },
-    Variables: {
-        userId: number
-    }
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  Variables: {
+    userId: number;
+  };
 }>();
 
-
-
-export async function createPost(c:Context) {
-	const user = Number(c.get('userId'));
-	const userId=Number(user);
-	if (isNaN(userId)) {
-    return c.json({ error: "Invalid or missing userId" }, 400);
-  }
-
-	const prisma = new PrismaClient({
-		datasourceUrl: c.env?.DATABASE_URL	,
-	}).$extends(withAccelerate());
-
-	const body = await c.req.json();
-	const post = await prisma.post.create({
-		data: {
-			title: body.title,
-			description: body.description,
-			userId:userId
-		}
-	});
-	return c.json({
-		id: post.id
-	});
+enum StatusCodes {
+  OK = 200,
+  BAD_REQUEST = 400,
+  UNAUTHORISED = 401,
+  NOT_FOUND = 404,
+  INTERNAL_SERVER_ERROR = 500,
 }
 
-export async function updatePostById(c:Context) {
-	const user = Number(c.get('userId'));
-	const userId=Number(user);
-	if (isNaN(userId)) {
-    return c.json({ error: "Invalid or missing userId" }, 400);
+export async function createPost(c: Context) {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const body = await c.req.json();
+    const parsedPost = postInputschema.safeParse(body);
+    if (!parsedPost.success) {
+      return c.json({ error: "invalid post" }, StatusCodes.BAD_REQUEST);
+    }
+    const { title, description } = parsedPost.data;
+
+    if (!title || !description) {
+      return c.json(
+        { error: "Please provide title, description" },
+        StatusCodes.BAD_REQUEST
+      );
+    }
+    const user = Number(c.get("userId"));
+    const userId = Number(user);
+    if (isNaN(userId)) {
+      return c.json(
+        { error: "Invalid or missing userId" },
+        StatusCodes.BAD_REQUEST
+      );
+    }
+    const post = await prisma.post.create({
+      data: {
+        title: body.title,
+        description: body.description,
+        userId: userId,
+      },
+    });
+    return c.json(
+      {
+        post,
+        id: post.id,
+      },
+      StatusCodes.OK
+    );
+  } catch (e) {
+    return c.json(
+      { error: `Error creating post.${e}` },
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
   }
-	const prisma = new PrismaClient({
-		datasourceUrl: c.env?.DATABASE_URL	,
-	}).$extends(withAccelerate());
-
-	const body = await c.req.json();
-	prisma.post.update({
-		where: {
-			id: body.id,
-			userId:userId
-		},
-		data: {
-			title: body.title,
-			description: body.description
-		}
-	});
-
-	return c.text('updated post');
 }
 
-export async function getPostById(c:Context) {
-	const rawId =c.req.param('id');
-	 const id = Number(rawId);
+export async function updatePostById(c: Context) {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const user = Number(c.get("userId"));
+    const userId = Number(user);
+    if (isNaN(userId)) {
+      return c.json({ error: "Invalid or missing userId" }, 400);
+    }
 
-  if (isNaN(id)) {
-    return c.json({ error: "Invalid ID" }, 400);
+    const body = await c.req.json();
+    const parsePost = postUpdateschema.safeParse(body);
+    if (!parsePost.success) {
+      return c.json({ error: "Invalid post input" }, StatusCodes.BAD_REQUEST);
+    }
+    const { title, description, id } = parsePost.data;
+
+    const post = prisma.post.update({
+      where: {
+        id: body.id,
+        userId: userId,
+      },
+      data: {
+        title: body.title,
+        description: body.description,
+      },
+    });
+    if (!post) {
+      return c.json({ msg: "Post not found" }, StatusCodes.NOT_FOUND);
+    }
+    return c.json({ post, message: "updated post" }, StatusCodes.OK);
+  } catch (e) {
+    return c.json(
+      { error: `Error updating post.${e}` },
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
   }
-	const prisma = new PrismaClient({
-		datasourceUrl: c.env?.DATABASE_URL	,
-	}).$extends(withAccelerate());
-	
-	const post = await prisma.post.findUnique({
-		where: {
-			id
-		}
-	});
-
-	return c.json(post);
 }
 
+export async function getPostById(c: Context) {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const rawId = c.req.param("id");
+    const id = Number(rawId);
+
+    if (isNaN(id)) {
+      return c.json({ error: "Invalid ID" }, 400);
+    }
+
+    const post = await prisma.post.findUnique({
+      where: {
+        id,
+      },
+	  include:{
+		User:true
+	  }
+    });
+    if (!post) {
+      return c.json({ msg: "Post not found" }, StatusCodes.NOT_FOUND);
+    }
+
+    return c.json(post);
+  } catch (e) {
+    return c.json(
+      { error: `Error retrieving post.${e}` },
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
 
 export async function getAllPosts(c: Context) {
   const prisma = new PrismaClient({
     datasourceUrl: c.env?.DATABASE_URL,
   }).$extends(withAccelerate());
+  try {
+    const getpost = await prisma.post.findMany({
+      include: {
+        User: true,
+      },
+    });
 
-  const getpost = await prisma.post.findMany({
-    include: {
-      User: true,
-    },
-  });
-
-  return c.json(getpost);
+    return c.json(getpost);
+  } catch (e) {
+    return c.json(
+      { error: `Error retrieving post.${e}` },
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
 }
--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -3,33 +3,59 @@ import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 import { Context } from "hono";
 import { sign } from "hono/jwt";
-
+import {signinschema ,signupschema}  from "../../../common/src";
 export const userRouter = new Hono<{
     Bindings: {
         DATABASE_URL: string;
         JWT_SECRET: string;
     }
 }>();
-
+enum StatusCodes{
+    OK = 200,
+    BAD_REQUEST = 400,
+    UNAUTHORISED = 401,
+    NOT_FOUND = 404,
+    INTERNAL_SERVER_ERROR = 500,
+}
 export async function signup(c:Context)  {
     const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate());
-  
-    const body = await c.req.json();
-  
+  try{
+    const body:{
+            username : string ,
+            email : string, 
+            password : string
+        }  = await c.req.json();
+  const {username , email , password} = body;
+ const parsedUser = signupschema.safeParse(body);
+   if(!parsedUser.success){
+            return c.json('Invalid user input', StatusCodes.BAD_REQUEST);
+        }
     const user = await prisma.user.create({
       data: {
-        email: body.email,
-        password: body.password,
+        username:username,
+        email: email,
+        password: password,
       },
     });
   
     const token = await sign({ id: user.id }, c.env.JWT_SECRET)
   
     return c.json({
-      jwt: token
-    })
+      msg: 'User created successfully ',
+      jwt: token,
+        user : {
+                userId : user.id,
+                username : user.username,
+                email : user.email
+            }
+            } ,StatusCodes.OK );
+    
+  }
+  catch(e){
+    return c.json(`Error creating user ${e}`, StatusCodes.INTERNAL_SERVER_ERROR);
+  }
 }
   
 export async function signin(c:Context) {
@@ -37,20 +63,42 @@ export async function signin(c:Context) {
     //@ts-ignore
         datasourceUrl: c.env?.DATABASE_URL	,
     }).$extends(withAccelerate());
+try{
+   const body : {
+            email : string, 
+            password  :string} = await c.req.json();
 
-    const body = await c.req.json();
+        const parsedUser = signinschema.safeParse(body);
+
+        //Zod error handling
+        if(!parsedUser.success){
+            return c.json('Invalid user input', StatusCodes.BAD_REQUEST);
+        }
+        const {email , password} = body;
     const user = await prisma.user.findUnique({
         where: {
-            email: body.email,
-    password: body.password
+            email: email,
+    password: password
         }
     });
 
     if (!user) {
-        c.status(403);
+        c.status(StatusCodes.NOT_FOUND);
         return c.json({ error: "user not found" });
     }
 
     const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-    return c.json({ jwt });
+    return c.json({msg:  'Signed in successfully', jwt,
+
+     user : {
+                    userId : user.id,
+                    username : user.username,
+                    email : user.email
+                }
+            }, 
+            StatusCodes.OK);
+  }
+  catch (error) {
+        return c.json(`Error signing in . ${error}`, StatusCodes.INTERNAL_SERVER_ERROR);       
+    }  
 }
