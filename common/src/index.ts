7a9872f5a163f4ea70e205b964b2132dd39bf2c6
Jeevika Sirwani <jeevika.sirwani4@gmail.com>
2025-05-20 15:03:18 +0530

    common folder added

---
+++ common/src/index.ts
@@ -0,0 +1,32 @@
+import {z} from 'zod';
+
+export const signupschema=z.object({
+    username:z.string().min(3).optional(),
+    email:z.string().email(),
+    password:z.string().min(6)
+})
+
+export const signinschema=z.object({
+    email:z.string().email(),
+    password:z.string().min(6)
+})
+
+
+export const postInputschema=z.object({
+    title:z.string(),
+    description:z.string(),
+})
+
+
+export const postUpdateschema=z.object({
+    title:z.string(),
+    description:z.string(),
+    id:z.number()
+})
+
+
+
+export type UpdatePostInput = z.infer<typeof postUpdateschema>;
+export type SignInInput = z.infer<typeof signinschema>;
+export type PostInput = z.infer<typeof postInputschema>;
+export type SignUpInput = z.infer<typeof signupschema>;
