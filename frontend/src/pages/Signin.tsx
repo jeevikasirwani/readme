e6f36c47b569c69a4191e4dff50d4232f9e69969
Jeevika Sirwani <jeevika.sirwani4@gmail.com>
2025-05-21 19:12:29 +0530

    auth added

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Auth from '../components/Auth'
+import Quote from '../components/Quote'
 
 function Signin() {
   return (
-    <div>Signin</div>
+    <div>
+      <div className='grid grid-cols-2'>
+        <div>
+          <Auth type='signin'/>
+        </div>
+        <div className='invisible lg:visible'>
+          <Quote/>
+        </div>
+      </div>
+    </div>
   )
 }
 
