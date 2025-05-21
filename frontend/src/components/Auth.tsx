114db894377226a7b399b4abd67e2cf815101851
Jeevika Sirwani <jeevika.sirwani4@gmail.com>
2025-05-21 15:44:14 +0530

    auth added

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,10 +1,11 @@
 import React, { type ChangeEvent } from "react";
 import { Link } from "react-router-dom";
 import { create } from "zustand";
-import type { SignUpInput } from '../../../common/src/index';
+import type { SignUpInput } from "../../../common/src/index";
 
 function Auth({ type }: { type: "signup" | "signin" }) {
-const {username,email,password,setUsername,setEmail,setPassword}=useAuthState();
+  const { username, email, password, setUsername, setEmail, setPassword } =
+    useAuthState();
   return (
     <div className="h-screen bg-white flex flex-col justify-center">
       <div className="flex justify-center">
@@ -19,18 +20,16 @@ const {username,email,password,setUsername,setEmail,setPassword}=useAuthState();
           </div>
         </div>
       </div>
-      {/* <div className="flex flex-col justify-center">
-        <p className="justify-center text-center">Username</p>
-        <input
-          className=" bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-300 rounded-md px-3 py-2 focus:outline-none focus:border-slate-400 hover:border-black shadow-sm focus:shadow"
-          placeholder="Enter Your Username"
-        />
-      </div> */}
+      <LabelledInput
+        label="Username"
+        placeholder="Enter your username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        id="username"
+      />
     </div>
   );
 }
-
-
 
 interface InputType {
   label: string;
@@ -48,7 +47,7 @@ function LabelledInput({
   id = "input",
 }: InputType) {
   return (
-    <div>
+    <div className="justify-center">
       <label
         htmlFor={id}
         className="mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -58,7 +57,7 @@ function LabelledInput({
       <input
         type="text"
         id={id}
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-700 dark:bg-gray-800 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-lg p-2.5 " 
         placeholder={placeholder}
         onChange={onChange}
         value={value}
@@ -68,18 +67,17 @@ function LabelledInput({
   );
 }
 
-
 interface AuthState extends SignUpInput {
   setUsername: (username: string) => void;
   setEmail: (email: string) => void;
   setPassword: (password: string) => void;
 }
-  export const useAuthState = create<AuthState>((set) => ({
-    username: "",
-    email: "",
-    password: "",
-    setUsername: (username) => set({ username }),
-    setEmail: (email) => set({ email }),
-    setPassword: (password) => set({ password }),
-  }));
+export const useAuthState = create<AuthState>((set) => ({
+  username: "",
+  email: "",
+  password: "",
+  setUsername: (username) => set({ username }),
+  setEmail: (email) => set({ email }),
+  setPassword: (password) => set({ password }),
+}));
 export default Auth;
